e7806c4a7e8a6aa38eedfccb9d6a83046295a725
Worki56 <65239379+Worki56@users.noreply.github.com>
2022-02-10 21:39:08 +0100

    V4
    Poprawa nazwy pliku z kodem, uczenie na mniejszych elementach, początek kierowania przez komendy

---
+++ main.py
@@ -0,0 +1,165 @@
+import os
+import cv2
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+dict_size = 128
+
+def czyst(zawartosc):
+    zawartosc1=''
+    for i in range(0, len(zawartosc)):
+        if zawartosc[i] != '\n' and zawartosc[i]!=' ':
+            zawartosc1=zawartosc1+zawartosc[i]
+    return zawartosc1
+
+def odczyt_danych_z_pliku(sciezk,sciezk1):
+    #przyjmująć konstrukcje podaną w przykładzie zapisu .xml
+    #można była skorzytać z pandas.read_xml ale wydawało mi się że jest to pójście na łatwizne
+    sciezka=sciezk+sciezk1
+    scie = sciezk + '/images/'
+    zawartosc_p2={}
+    plik = open(sciezka, "r", encoding="utf-8")
+    zawartosc_p1= plik.read()
+    zawartosc_p1=czyst(zawartosc_p1)
+    wyraz_tyczas = ''
+    ile_object=0
+    lacz=[]
+    for i in range(0, len(zawartosc_p1)):
+        co=zawartosc_p1[i]
+        if zawartosc_p1[i] == '<':
+            if zawartosc_p1[i+1] == '/':
+                laczw =''
+                for n in lacz:
+                    laczw=laczw+n+'.'
+                if wyraz_tyczas!='':
+                    zawartosc_p2[laczw]=wyraz_tyczas
+                wyraz_tyczas=''
+                del lacz[-1]
+        elif zawartosc_p1[i] == '>':
+            if wyraz_tyczas[0]!='/' and wyraz_tyczas[0]!='':
+                if wyraz_tyczas=='object':
+                    lacz.append(wyraz_tyczas+str(ile_object))
+                    ile_object+=1
+                else:
+                    lacz.append(wyraz_tyczas)
+            wyraz_tyczas=''
+
+        else:
+            wyraz_tyczas = wyraz_tyczas + zawartosc_p1[i]
+    zawartosc_p2['ile_object'] = ile_object
+    if ile_object != 0:
+        image = cv2.imread(scie+zawartosc_p2['annotation.filename.'])
+    for i in range(0, ile_object):
+        if zawartosc_p2['annotation.object'+str(i)+'.name.']=="speedlimit":
+            zawartosc_p2['annotation.object' + str(i)+'.name.']='1'
+        else:
+            zawartosc_p2['annotation.object' + str(i)+'.name.'] = '0'
+        zawartosc_p2['annotation.object' + str(i) + '.image.array.']=image[(int(zawartosc_p2['annotation.object' + str(i) + '.bndbox.ymin.'])-1):(int(zawartosc_p2['annotation.object' + str(i) + '.bndbox.ymax.'])-1),(int(zawartosc_p2['annotation.object' + str(i) + '.bndbox.xmin.'])-1):(int(zawartosc_p2['annotation.object' + str(i) + '.bndbox.xmax.'])-1)]
+    return zawartosc_p2
+
+def odczyt_danych_z_folderu(folder):
+    nazwa_f_z_inf="annotations"
+    lista_plikow = os.listdir(folder+'/'+nazwa_f_z_inf)
+    zawartosc=[]
+    for n in lista_plikow:
+        zawartosc.append(odczyt_danych_z_pliku(folder,'/'+nazwa_f_z_inf+'/'+n))
+    return zawartosc
+
+def uczenie(dane,sciezka):
+    bow = cv2.BOWKMeansTrainer(dict_size)
+    przesiew = cv2.SIFT_create()
+    for przy in dane:
+        for i in range(0, przy['ile_object']):
+            kpts = przesiew.detect(przy['annotation.object' + str(i) + '.image.array.'], None)
+            kpts, desc = przesiew.compute(przy['annotation.object' + str(i) + '.image.array.'], kpts)
+            if desc is not None:
+                bow.add(desc)
+    slownik = bow.cluster()
+    np.save(sciezka+'/slow.npy', slownik)
+
+def wyodrebienie(dane,sciezka):
+
+    przesiew = cv2.SIFT_create()
+    flann = cv2.FlannBasedMatcher_create()
+    bow = cv2.BOWImgDescriptorExtractor(przesiew, flann)
+    slownik = np.load(sciezka+'/slow.npy')
+    bow.setVocabulary(slownik)
+    for przy in dane:
+        for i in range(0, przy['ile_object']):
+            kpts = przesiew.detect(przy['annotation.object' + str(i) + '.image.array.'], None)
+            desc = bow.compute(przy['annotation.object' + str(i) + '.image.array.'], kpts)
+            if desc is not None:
+                przy['desc'+ str(i)]=desc
+            else:
+                przy['desc' + str(i)] = np.zeros((1, dict_size))
+    return dane
+
+def wyodrebienie2(dane,sciezka):
+
+    przesiew = cv2.SIFT_create()
+    flann = cv2.FlannBasedMatcher_create()
+    bow = cv2.BOWImgDescriptorExtractor(przesiew, flann)
+    slownik = np.load(sciezka+'/slow.npy')
+    bow.setVocabulary(slownik)
+
+    for przy in dane:
+        kpts = przesiew.detect(sample['image'], None)
+        desc = bow.compute(sample['image'], kpts)
+        if desc is not None:
+            przy['desc']=desc
+        else:
+            przy['desc'] = np.zeros((1, dict_size))
+    return dane
+
+def trenowanie(dane):
+    clf = RandomForestClassifier(dict_size)
+    x = np.empty((1, dict_size))
+    y = []
+    for przy in dane:
+        for i in range(0, przy['ile_object']):
+            y.append(przy['annotation.object' + str(i)+'.name.'])
+            x = np.vstack((x, przy['desc'+ str(i)]))
+    clf.fit(x[1:], y)
+    return clf
+
+def predykcja(rf, dane):
+    #do poprawy
+    for przy in dane:
+        przy['label_pred']=rf.predict(przy['desc'])[0]
+    return dane
+
+def wypisz(sciezka):
+    nazwa="wynik.txt"
+    sciezka=sciezka+'/'+nazwa
+    plik = open(sciezka, "r", encoding="utf-8")
+    zawartosc_p1 = plik.read()
+    print(zawartosc_p1)
+
+def klasyfikacja():
+
+    return True
+
+def main():
+    # Przyjmująć że plik zanjduje się jak w przykładzie
+    gdzie="test"
+    gdzie2 = "train"
+    os.chdir("..")
+    dane_z_plików = odczyt_danych_z_folderu(gdzie)
+    # zapisuje plik w folderze "Test"
+    uczenie(dane_z_plików, gdzie)
+    wyodrebienie(dane_z_plików, gdzie)
+    rf = trenowanie(dane_z_plików)
+    #del dane_z_plików
+    #dane_z_plików = predykcja(rf, dane_z_plików)
+    while True:
+        funkcja = input()
+        if funkcja == "classify":
+            klasyfikacja()
+        elif funkcja == "detect":
+            wypisz(gdzie2)
+        else:
+            print("Error")
+
+if __name__ == '__main__':
+    main()
+
